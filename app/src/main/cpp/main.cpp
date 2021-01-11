71c47ba030043fc2cce0b4c705cc608f6f240db3
simondai <simon_dai@zhan.com>
2021-01-11 14:07:53 +0800

    哈夫曼编码实现

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,33 +1,19 @@
-//
-// Created by simonday on 2020/12/24.
-//
-
-#include "Hello.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "huffman.h"
 
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_simon_a_jni_JNICard_cardOrc(JNIEnv *env, jclass clazz, jobject bitmap) {
-
-
-
+int main(void)
+{
 	//We build the tree depending on the string
-	// 生成一棵最优的前缀码霍夫曼树
-	htTree *codeTree = buildTree("aaaabbcccccccd");
+	htTree *codeTree = buildTree("I like drinking beer!");
 	//We build the table depending on the Huffman tree
-	// 根据霍夫曼树生成映射表
 	hlTable *codeTable = buildTable(codeTree);
 
 	//We encode using the Huffman table
-	encode(codeTable,"aaaabbcccccccd");
+	encode(codeTable,"I like drinking beer!");
 	//We decode using the Huffman tree
 	//We can decode string that only use symbols from the initial string
 	decode(codeTree,"0011111000111");
 	//Output : 0011 1110 1011 0001 0010 1010 1100 1111 1000 1001
-
-	return env->NewStringUTF("6888");
-
-
-
+	return 0;
 }
